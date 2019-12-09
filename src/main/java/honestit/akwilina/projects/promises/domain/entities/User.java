36d1c129106c0221014d12e391c97e0875b6ac73
honestit <michal.kupisinski@gmail.com>
2019-12-09 15:26:38 +0100

    Create user entity

---
+++ src/main/java/honestit/akwilina/projects/promises/domain/entities/User.java
@@ -0,0 +1,28 @@
+package honestit.akwilina.projects.promises.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@ToString(exclude = "password") @EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private Boolean active = Boolean.FALSE;
+    @Column(nullable = false)
+    private String password;
+}
