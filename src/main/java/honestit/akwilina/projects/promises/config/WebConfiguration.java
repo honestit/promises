afc49b2847a7fccc44ac79b61fd080c8e1034e49
akwilina <aleksandra.man@gmail.com>
2019-12-10 13:57:33 +0100

    Changing LoginController class to addViewControllers registry

---
+++ src/main/java/honestit/akwilina/projects/promises/config/WebConfiguration.java
@@ -0,0 +1,16 @@
+package honestit.akwilina.projects.promises.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
