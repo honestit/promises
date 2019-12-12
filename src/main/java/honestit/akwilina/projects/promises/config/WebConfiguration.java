458e9285f75ab69684778124e177e78678841bb2
akwilina <aleksandra.man@gmail.com>
2019-12-12 09:07:15 +0100

    Adding i18n validation messages to registration form

--- src/main/java/honestit/akwilina/projects/promises/config/WebConfiguration.java
+++ src/main/java/honestit/akwilina/projects/promises/config/WebConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
