5e231968dd238ea825f7847afab730744df206c1
akwilina <aleksandra.man@gmail.com>
2019-12-12 11:32:51 +0100

    Added friend repo with method to find a friend

---
+++ src/main/java/honestit/akwilina/projects/promises/domain/repositories/FriendRepository.java
@@ -0,0 +1,9 @@
+package honestit.akwilina.projects.promises.domain.repositories;
+
+import honestit.akwilina.projects.promises.domain.entities.Friend;
+import honestit.akwilina.projects.promises.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    Friend getOneByOwnerAndName(User user, String whom);
+}
