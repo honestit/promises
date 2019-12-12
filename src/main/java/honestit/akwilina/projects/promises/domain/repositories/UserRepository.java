7cb79aa3148776582c42689f37d1e7fef067e715
akwilina <aleksandra.man@gmail.com>
2019-12-12 11:32:11 +0100

    Added getByUsername method in repo

--- src/main/java/honestit/akwilina/projects/promises/domain/repositories/UserRepository.java
+++ src/main/java/honestit/akwilina/projects/promises/domain/repositories/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User getByUsername(String username);
+
     
 }
