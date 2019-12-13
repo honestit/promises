6d6eeaf0e591ac4bf0030a133d3eadb6bbccee3b
akwilina <aleksandra.man@gmail.com>
2019-12-13 06:44:50 +0100

    Added setup data creator with roles, for heroku profile

---
+++ src/main/java/honestit/akwilina/projects/promises/setup/StarterDataCreator.java
@@ -0,0 +1,34 @@
+package honestit.akwilina.projects.promises.setup;
+
+import honestit.akwilina.projects.promises.domain.entities.Role;
+import honestit.akwilina.projects.promises.domain.repositories.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Profile("heroku") @Slf4j @RequiredArgsConstructor
+public class StarterDataCreator implements ApplicationRunner {
+
+    private final RoleRepository roleRepository;
+
+    @Override @Transactional
+    public void run(ApplicationArguments args) throws Exception {
+        createRoles();
+    }
+
+    private void createRoles() {
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+    }
+}
