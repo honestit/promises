b79dce972b386e7c07e27fd55bd1d18dc23553e0
akwilina <aleksandra.man@gmail.com>
2019-12-13 09:32:22 +0100

    Added additional constraints to registrationDataDTO

--- src/main/java/honestit/akwilina/projects/promises/dtos/RegistrationDataDTO.java
+++ src/main/java/honestit/akwilina/projects/promises/dtos/RegistrationDataDTO.java
@@ -1,15 +1,19 @@
 package honestit.akwilina.projects.promises.dtos;
 
+import honestit.akwilina.projects.promises.validation.constraints.SamePasswords;
+import honestit.akwilina.projects.promises.validation.constraints.UniqueEmail;
+import honestit.akwilina.projects.promises.validation.constraints.UniqueUsername;
+import honestit.akwilina.projects.promises.validation.groups.BusinessLogic;
 import lombok.Data;
 
 import javax.validation.constraints.*;
 
-@Data
+@Data @SamePasswords
 public class RegistrationDataDTO {
 
-    @NotBlank @Size(min = 3, max = 12)
+    @NotBlank @Size(min = 3, max = 12) @UniqueUsername(groups = BusinessLogic.class)
     private String username;
-    @NotBlank @Email
+    @NotBlank @Email @UniqueEmail(groups = BusinessLogic.class)
     private String email;
     @NotBlank @Size(min = 4, max = 12)
     private String password;
