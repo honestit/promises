eb0e385e3de362cf36734761db845c219ac66096
honestit <michal.kupisinski@gmail.com>
2020-05-22 09:05:53 +0200

    Added test user for dev and heroku profile

--- src/main/java/honestit/akwilina/projects/promises/setup/StarterDataCreator.java
+++ src/main/java/honestit/akwilina/projects/promises/setup/StarterDataCreator.java
@@ -1,12 +1,15 @@
 package honestit.akwilina.projects.promises.setup;
 
 import honestit.akwilina.projects.promises.domain.entities.Role;
+import honestit.akwilina.projects.promises.domain.entities.User;
 import honestit.akwilina.projects.promises.domain.repositories.RoleRepository;
+import honestit.akwilina.projects.promises.domain.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class StarterDataCreator implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
@@ -32,5 +37,15 @@ public class StarterDataCreator implements ApplicationRunner {
         Role adminRole = new Role();
         adminRole.setName("ROLE_ADMIN");
         roleRepository.save(adminRole);
+    }
+
+    private void createUsers() {
+        User user = new User();
+        user.setUsername("user");
+        user.setEmail("user@test.mail");
+        user.setPassword(passwordEncoder.encode("pass"));
+        user.setActive(true);
+        user.getRoles().add(roleRepository.getByName("ROLE_USER"));
+        userRepository.save(user);
     }
 }
