8d4b15b40076337ec6c0224da9ed2618d38aa2ff
honestit <michal.kupisinski@gmail.com>
2020-05-22 08:43:24 +0200

    Some cleanup in properties and new dev profile

--- src/main/java/honestit/akwilina/projects/promises/setup/StarterDataCreator.java
+++ src/main/java/honestit/akwilina/projects/promises/setup/StarterDataCreator.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Profile("heroku")
+@Profile({"heroku", "dev"})
 @Slf4j
 @RequiredArgsConstructor
 public class StarterDataCreator implements ApplicationRunner {
